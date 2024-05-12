cd52345bab60f0f868a30aa701df7bc667da5d69
Aarian <arimorteza@gmail.com>
2024-05-12 03:18:40 -0400

    adding publication.js

---
+++ assets/js/publication.js
@@ -0,0 +1,9 @@
+// JavaScript for toggling and showing/hiding content
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
